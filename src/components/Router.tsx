9483af459a44020881e1303ec61ce65d1fbc0e9b
EdineiAndrade <edinei22.ba@gmail.com>
2022-06-23 18:13:11 -0300

    Aula 03:router

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -4,8 +4,6 @@ import { Event } from "../pages/Event";
 export function Router(){
     return(
         <Routes>
-            <Route path="/" element={<h1>Home</h1>}/>
-            <Route path="/event" element={<Event/>}/>
             <Route path="/event/lesson/:slug" element={<Event/>}/>
         </Routes>
     )
