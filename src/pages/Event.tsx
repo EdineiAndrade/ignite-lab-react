726270e74fdb1315546457fdfc7a72a6037f3be5
EdineiAndrade <edinei22.ba@gmail.com>
2022-07-17 18:05:13 -0300

    styled pages

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -10,13 +10,13 @@ export function Event() {
   return(
     <div className="md:flex md:flex-col min-h-screen" >  
          <Heder/>
-         <main className="md:flex md:flex-1">
+         <main className="mx-2 md:flex md:flex-1">
           {slug 
           ? <Video lessonSlug={slug}/> 
           : <div className="flex-1"> 
-              <div className={` ${slug ? 'hidden display:none' : 'flex w-full h-[600px] justify-center items-center' }`}>
+              <div className={` ${slug ? 'hidden display:none' : 'flex md:w-full md:h-[600px] justify-center items-center' }`}>
                 
-                  <div className="text-xl font-bold h-[60px] w-full md:text-2xl transition-colors border md:mx-50 md:rounded-2xl bg-purple-600 md:w-[520px] md:h-[100px] border-gray-900 text-gray-700 border-spacing-4 mt-100 flex justify-center items-center md:justify-between md:p-8 md:m-8">
+                  <div className="text-xl font-bold w-full h-[60px] md:text-2xl transition-colors border md:mx-50 md:rounded-2xl bg-purple-600 md:w-[520px] md:h-[100px] border-gray-900 text-gray-700 border-spacing-4 mt-100 flex justify-center items-center md:justify-between md:p-8 md:m-8">
 
                     <h1>Escolha uma Aula parra assistir</h1>
                     <CaretDoubleRight size={35} className="hidden display:none md:flex md:flex-color"/>
